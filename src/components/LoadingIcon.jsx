66ca7d43a6559528618f06379fb4428f84e22e17
socialtonics <mjkgarrow@gmail.com>
2023-07-20 18:34:09 +1000

    added a loading animation

---
+++ src/components/LoadingIcon.jsx
@@ -0,0 +1,8 @@
+
+export default function LoadingIcon() {
+    return (
+        <div className='w-full h-screen flex justify-center'>
+            <span className="loading loading-dots loading-lg m-auto"></span>
+        </div>
+    )
+}
