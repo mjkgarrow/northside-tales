5763ecf638bed629d87d5b40cb116cc179548f1b
socialtonics <mjkgarrow@gmail.com>
2023-07-20 23:50:54 +1000

    restyling navbar so it isn't so big

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -30,13 +30,13 @@ export default function NavBar() {
 
 
     return (
-        <header className="navbar bg-base-100 p-4 flex-col sm:flex-row">
-            <div className="flex-1 min-w-fit pb-3 pl-5 sm:pb-0">
-                <a className="normal-case text-xl font-bold">Northside tales</a>
+        <header className="navbar bg-base-100 p-2 sm:p-4 flex-col sm:flex-row">
+            <div className="flex-1 min-w-fit sm:pl-4">
+                <a className="normal-case text-2xl font-bold">Northside tales</a>
             </div>
-            <div className="flex-none gap-2 py-5">
+            <div className="flex-none gap-2 py-0 sm:py-5">
                 <div className="dropdown z-[9999]">
-                    <label tabIndex={0} className="btn m-1" onClick={handleClick}>Message type</label>
+                    <label tabIndex={0} className="btn" onClick={handleClick}>Markers</label>
                     <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
                         <li><button onClick={changeFilter} value="">All markers</button></li>
                         <li><button onClick={changeFilter} value="vibe">Vibes</button></li>
