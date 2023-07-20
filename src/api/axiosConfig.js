d90c156fd232e73518f54415a322a70cf879d87f
socialtonics <mjkgarrow@gmail.com>
2023-07-20 20:29:55 +1000

    added environment variable to connect to backend

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
+const URL = import.meta.env.VITE_API_HOST || 'http://localhost:3000'
+
 // Create an axios instance with custom configuration
 const api = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: URL,
 })
 
 export default api
