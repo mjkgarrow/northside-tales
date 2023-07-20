5495e8ee0095f68420fd6d10dd08e2b8aead6ed4
socialtonics <mjkgarrow@gmail.com>
2023-07-21 01:01:50 +1000

    styling change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="footer footer-center p-2 bg-base-100 text-base-content justify-end">
+        <footer className="footer footer-center p-2 bg-base-100 text-base-content justify-end sm:p-6">
             <div className="flex gap-4 justify-center items-center">
                 <p>Created by <a href='https://github.com/mjkgarrow' className='underline' target="_blank">Matt Garrow</a></p>
             </div>
