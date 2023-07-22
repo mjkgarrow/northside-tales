b3a344b4b23b5e3fa2446f814c68ad00bdfcba1c
socialtonics <mjkgarrow@gmail.com>
2023-07-22 14:21:39 +1000

    styling changes and helper text to close modal

--- src/components/MapForm.jsx
+++ src/components/MapForm.jsx
@@ -26,39 +26,47 @@ export default function MapForm({ latLng }) {
         // Add marker to global state
         createMarker.mutate(data)
 
-        // Reset form values
-        reset()
-
         // Close modal
-        window.meeting_modal_create.close()
+        window.marker_form_modal.close()
 
         // Clear temporary marker
         setTempMarker(false)
 
-        // Reset textarea height after 200 milliseconds so it doesn't look janky
-        setTimeout(function () { document.getElementById("formTextArea").style.height = "102px" }, 200)
+        // Reset form after 200 milliseconds so it doesn't look janky
+        setTimeout(function () {
+            // Reset form values
+            reset()
+
+            // Resent form height
+            document.getElementById("formTextArea").style.height = "102px"
+        }, 200)
     }
 
     // On modal close
     const clearModal = () => {
-        // Reset form values
-        reset()
-
         // Clear temporary marker
         setTempMarker(false)
 
-        // Reset textarea height after 200 milliseconds so it doesn't look janky
-        setTimeout(function () { document.getElementById("formTextArea").style.height = "102px" }, 200)
+        // Reset form after 200 milliseconds so it doesn't look janky
+        setTimeout(function () {
+            // Reset form values
+            reset()
+
+            // Resent form height
+            document.getElementById("formTextArea").style.height = "102px"
+        }, 200)
     }
 
     return (
-        <dialog id='meeting_modal_create' className='modal'>
+        <dialog id='marker_form_modal' className='modal'>
             <form
                 autoComplete='off'
                 onSubmit={handleSubmit(onSubmit)}
                 method='dialog'
                 className='modal-box rounded-lg w-60 sm:w-72'>
                 <div className='flex flex-col gap-3 sm:gap-4'>
+                    <p className="text-lg text-center text-violet-500">Leave a message!</p>
+
                     {/* Name input */}
                     <input
                         {...register('name', {
@@ -89,13 +97,13 @@ export default function MapForm({ latLng }) {
                         {...register('type', {
                             required: 'Type required',
                         })}
-                        className='select select-bordered select-sm sm:select-md w-full'
+                        className='select select-bordered select-sm sm:select-md w-full font-light'
                         required
                         defaultValue=""
                         data-form-type="other"
                         autoComplete='off'>
 
-                        <option value="" disabled>Message type</option>
+                        <option value="" disabled className="">Message type</option>
                         <option value="Vibe">Vibe</option>
                         <option value="Goss">Goss</option>
                         <option value="Deeds">Deeds</option>
@@ -105,10 +113,13 @@ export default function MapForm({ latLng }) {
                 </div>
 
                 <div className='modal-action mt-3 sm:mt-4'>
-                    <button className='btn btn-accent btn-sm sm:btn-md w-full normal-case'>
+                    <button className='btn btn-accent btn-outline btn-sm sm:btn-md w-full normal-case'>
                         Add message!
                     </button>
                 </div>
+
+                <p className="text-sm mt-3 ml text-center">Click anywhere to close</p>
+
             </form>
 
             {/* second form with 'modal-backdrop' class covers the screen so we can close the modal when clicked outside */}
