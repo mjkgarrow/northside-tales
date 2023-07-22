af8101a2e3362d2b1ce998f2d8b76f929e4c2dfc
socialtonics <mjkgarrow@gmail.com>
2023-07-22 17:17:55 +1000

    added additional map style selection

--- src/components/MapCanvas.jsx
+++ src/components/MapCanvas.jsx
@@ -1,16 +1,15 @@
 
 
-import { MapContainer, Popup, Marker, useMapEvents, useMap } from 'react-leaflet'
+import { MapContainer, Popup, Marker } from 'react-leaflet'
 import L from "leaflet";
 import { useEffect, useState } from 'react'
 import { useGlobalState, useReactQueries } from '../context/globalState'
-import { DarkCleanMap, LightCleanMap, mapCanvasProps, customIcon, createCustomClusterIcon } from './MapAssets'
-import { convertToNaturalLanguage, convertDateInput, cleanDateString } from '../utils/helpers'
+import { DarkCleanMap, LightCleanMap, mapCanvasProps, customIcon, Watercolour } from './MapAssets'
+import { convertToNaturalLanguage } from '../utils/helpers'
 
 import 'leaflet/dist/leaflet.css'
 import "leaflet-easybutton/src/easy-button.js";
 import "leaflet-easybutton/src/easy-button.css";
-import "font-awesome/css/font-awesome.min.css";
 
 export default function MapCanvas() {
     // Access global state
@@ -27,6 +26,9 @@ export default function MapCanvas() {
 
     // Manage user geolocation
     const [userLoc, setUserLoc] = useState(null)
+
+    // Manage watercolour map button
+    const [water, setWater] = useState(null)
 
     // Refresh map on theme change
     useEffect(() => {
@@ -55,7 +57,7 @@ export default function MapCanvas() {
         // Confirm the map is loaded
         if (!map) return;
 
-        // When map is clicked, close open popups or open modal
+        // Add click functionm to map, close open popups or open modal
         map.on("click", (event) => {
 
             // Set latLng data to global context
@@ -72,14 +74,39 @@ export default function MapCanvas() {
 
                 // Fly map to clicked location
                 map.flyTo({
-                    lat: event.latlng.lat - 0.001,
+                    lat: event.latlng.lat - (map.getMaxZoom() > 17 ? 0.001 : 0.002),
                     lng: event.latlng.lng
-                }, 18)
+                }, map.getMaxZoom() > 17 ? 18 : 17)
 
                 // Open form modal
                 window.marker_form_modal.showModal()
             }
         })
+
+        // Button to reveal watercolour map
+        L.easyButton({
+            states: [{
+                stateName: 'watercolour-map',
+                icon: '<div class="h-full flex justify-center items-center"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M384 476.1L192 421.2V35.9L384 90.8V476.1zm32-1.2V88.4L543.1 37.5c15.8-6.3 32.9 5.3 32.9 22.3V394.6c0 9.8-6 18.6-15.1 22.3L416 474.8zM15.1 95.1L160 37.2V423.6L32.9 474.5C17.1 480.8 0 469.2 0 452.2V117.4c0-9.8 6-18.6 15.1-22.3z"/></svg></div>',
+                title: 'Watercolour map!',
+                onClick: function (btn, map) {
+                    setWater(true)
+                    map.setZoom(map.getZoom() < 17 ? 15 : map.getZoom())
+                    map.setMaxZoom(17)
+
+                    btn.state('regular-map')
+                }
+            }, {
+                stateName: 'regular-map',
+                icon: '<div class="h-full flex justify-center items-center"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M565.6 36.2C572.1 40.7 576 48.1 576 56V392c0 10-6.2 18.9-15.5 22.4l-168 64c-5.2 2-10.9 2.1-16.1 .3L192.5 417.5l-160 61c-7.4 2.8-15.7 1.8-22.2-2.7S0 463.9 0 456V120c0-10 6.1-18.9 15.5-22.4l168-64c5.2-2 10.9-2.1 16.1-.3L383.5 94.5l160-61c7.4-2.8 15.7-1.8 22.2 2.7zM48 136.5V421.2l120-45.7V90.8L48 136.5zM360 422.7V137.3l-144-48V374.7l144 48zm48-1.5l120-45.7V90.8L408 136.5V421.2z"/></svg></svg></div>',
+                title: 'Regular map',
+                onClick: function (btn, map) {
+                    setWater(false)
+                    map.setMaxZoom(20)
+                    btn.state('watercolour-map')
+                }
+            }]
+        }).addTo(map);
 
         // Check geolocation is supported
         if (navigator.geolocation) {
@@ -95,15 +122,15 @@ export default function MapCanvas() {
                 loadingLoc.remove(map)
 
                 // Add show-location button
-                L.easyButton("fa-location-arrow", () => {
+                L.easyButton('<div class="h-full flex justify-center items-center"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M429.6 92.1c4.9-11.9 2.1-25.6-7-34.7s-22.8-11.9-34.7-7l-352 144c-14.2 5.8-22.2 20.8-19.3 35.8s16.1 25.8 31.4 25.8H224V432c0 15.3 10.8 28.4 25.8 31.4s30-5.1 35.8-19.3l144-352z"/></svg></div>', () => {
                     navigator.geolocation.getCurrentPosition((position) => {
-                        map.flyTo({ lat: position.coords.latitude, lng: position.coords.longitude }, 18, { animation: false });
+                        map.flyTo({ lat: position.coords.latitude, lng: position.coords.longitude }, map.getMaxZoom() > 17 ? 18 : 17, { animation: false });
                     }, () => {
                         console.log("Unable to retrieve your location")
                     }, {
                         enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity
                     })
-                }).addTo(map)
+                }, 'Show your location').addTo(map)
 
             }, () => { // Error function to call if location services disabled
                 loadingLoc.remove(map)
@@ -164,7 +191,8 @@ export default function MapCanvas() {
                 className='h-full w-11/12 rounded-xl antialiased shadow-2xl shadow-gray-600 cursor-pointer focus-visible:'>
 
                 {/* Change map based on theme */}
-                {theme === 'dark' ? DarkCleanMap : LightCleanMap}
+                {water ? Watercolour : theme === 'dark' ? DarkCleanMap : LightCleanMap}
+                {/* {theme === 'dark' ? DarkCleanMap : LightCleanMap} */}
 
                 {/* Show temp marker when user clicks on map */}
                 {tempMarker && <Marker position={tempMarker} icon={customIcon("")} />}
