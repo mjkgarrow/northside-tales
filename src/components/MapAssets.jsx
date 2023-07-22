63b8b106e6b142aa41b9f0d0d08c450bf137ae0a
socialtonics <mjkgarrow@gmail.com>
2023-07-22 17:11:24 +1000

    changed temp marker icon style

--- src/components/MapAssets.jsx
+++ src/components/MapAssets.jsx
@@ -9,7 +9,8 @@ export const Pioner = <TileLayer
 
 export const Watercolour = <TileLayer
     attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>'
-    url='https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg' />
+    url='https://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.jpg'
+    maxZoom={17} />
 
 export const Classic = <TileLayer
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -65,7 +66,7 @@ export const customIcon = (type) => {
             icon = '<svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" class="drop-shadow-glow"><circle cx="16" cy="16" r="8" class="fill-blue-500"><animate attributeName="r" values="8;9.5;8" keyTimes="0;0.5;1" dur="4s" repeatCount="indefinite" /></circle><circle cx="16" cy="16" r="5" class="fill-blue-800"><animate attributeName="r" values="3;4.5;3" keyTimes="0;0.5;1" dur="4s" repeatCount="indefinite" /></circle></svg>'
             break;
         default:
-            icon = '<svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12 10c-1.104 0-2-.896-2-2s.896-2 2-2 2 .896 2 2-.896 2-2 2m0-5c-1.657 0-3 1.343-3 3s1.343 3 3 3 3-1.343 3-3-1.343-3-3-3m-7 2.602c0-3.517 3.271-6.602 7-6.602s7 3.085 7 6.602c0 3.455-2.563 7.543-7 14.527-4.489-7.073-7-11.072-7-14.527m7-7.602c-4.198 0-8 3.403-8 7.602 0 4.198 3.469 9.21 8 16.398 4.531-7.188 8-12.2 8-16.398 0-4.199-3.801-7.602-8-7.602" class="fill-gray-400" /></svg>'
+            icon = '<svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><circle cx="16" cy="16" r="8" class="fill-emerald-500"></circle><circle cx="16" cy="16" r="5" class="fill-emerald-800"></circle></svg>'
             break;
     }
 
@@ -74,13 +75,5 @@ export const customIcon = (type) => {
         html: icon,
         iconAnchor: [12, 25],
         popupAnchor: [-4, -32]
-    })
-}
-
-export const createCustomClusterIcon = (cluster) => {
-    return new divIcon({
-        html: `<div>${cluster.getChildCount()}</div>`,
-        className: "bg-teal-600 text-white text-2xl font-bold text-center rounded-full h-10 w-10 relative -left-1/2 -top-5 flex justify-center items-center",
-        iconSize: []
     })
 }
