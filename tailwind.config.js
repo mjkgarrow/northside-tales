11f58d604054c709f269008cb0f6607be86d8961
socialtonics <mjkgarrow@gmail.com>
2023-07-22 14:20:33 +1000

    extended theme to use glow effect on user location icon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      dropShadow: {
+        glow: [
+          "0 0px 8px rgba(0,0, 255, 0.6)",
+          "0 0px 20px rgba(0, 0,255, 0.4)",
+        ],
+      },
       height: {
         screen: ['100vh', '100dvh'],
       }
